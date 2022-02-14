671ea6337b9edf0c6bae6abe108fff65670e842a
Bogdan748 <andriesbogdan00@gmail.com>
2022-02-14 19:06:41 +0200

    Simple example, 2 constructors

---
+++ OopConstructors/OopConstructors/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopConstructors
+{
+    public class Person
+    {
+        public Person()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            DateOfBirth = new DateTime(1950, 1, 1);
+        }
+
+
+
+
+            public Person(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"{this.FirstName}||{this.LastName}||{this.DateOfBirth}");
+        }
+    }
+}
---
+++ OopConstructors/OopConstructors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OopConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("John","Doe",new DateTime(1980,2,14));
+
+            Person d = new Person();
+
+            d.Print();
+        }
+    }
+}
